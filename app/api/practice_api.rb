ea57468b5c9f130074d3e780b9c1f53be370f7ad
tavern <windack@qq.com>
2015-01-21 17:05:45 +0800

    跨域请求header重写

--- app/api/practice_api.rb
+++ app/api/practice_api.rb
@@ -6,7 +6,7 @@ class PracticeAPI < Grape::API
   after do
     header 'Access-Control-Allow-Origin', '*'
     header 'Access-Control-Allow-Methods', '*'
-    header 'Access-Control-Allow-Headers', 'x-requested-with'
+    header 'Access-Control-Allow-Headers', 'Range-Unit,Range'
   end
 
   rescue_from :all do |e|
