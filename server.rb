0cad26a86148b451bc27573f513ab9fcd19b19ff
Tavern <windack@qq.com>
2015-01-22 14:32:04 +0800

    修改cookie有效区域

--- server.rb
+++ server.rb
@@ -11,9 +11,10 @@ class Application < Goliath::API
   def response(env)
     result = ::PracticeAPI.call(env)
     headers= result[1]
-    headers['Access-Control-Allow-Origin'] = 'localhost,127.0.0.1'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Range-Unit,Range,Content-Type'
+    headers['Access-Control-Allow-Credentials'] = 'true'
     result
   end
 end
