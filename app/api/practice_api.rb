207ca46985a37b3a9f524132c8ff184371855ae4
tavern <windack@qq.com>
2015-01-28 12:55:07 +0800

    修改绑定

--- app/api/practice_api.rb
+++ app/api/practice_api.rb
@@ -3,8 +3,16 @@ class PracticeAPI < Grape::API
   format :json
   helpers ::ToolKit
 
+  before do
+    env[:require_domain] = require_domain = headers['Referer']
+    header 'Access-Control-Allow-Origin', require_domain[0...require_domain.to_s.index(?/, 7)]
+  end
+
   rescue_from :all do |e|
-    error_response status: 500, message: {message: e.message, class: e.class.name, statck: e.backtrace}.as_json
+    require_domain = env[:require_domain]
+    # error!({message: e.message, class: e.class.name, statck: e.backtrace}.as_json,500, {:'Access-Control-Allow-Origin' => require_domain[0...require_domain.to_s.index(?/, 7)]})
+    error_response status: 500, message: {message: e.message, class: e.class.name, statck: e.backtrace}.as_json,
+                   headers: {'Access-Control-Allow-Origin' => require_domain[0...require_domain.to_s.index(?/, 7)]}
   end
 
   require "#{__dir__}/user_api"
@@ -21,12 +29,12 @@ class PracticeAPI < Grape::API
       #领域用户
       @space_user= User.find_or_reg(params[:space_name], false)
       #当前登录用户
-      @current_user= auth_with_cookie_or_header #  User.auth_with_cookie cookies
+      @current_user= User.auth_with_cookie cookies
     end
 
-    mount PracticeAPI::UserApi #=> '/'
-    mount PracticeAPI::ArticleApi #=> '/'
-    mount PracticeAPI::TagApi #=> '/'
+    mount PracticeAPI::UserApi => '/'
+    mount PracticeAPI::ArticleApi => '/'
+    mount PracticeAPI::TagApi => '/'
   end
 
 end
